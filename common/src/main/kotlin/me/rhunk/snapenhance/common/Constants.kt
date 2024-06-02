e9bbf365ac9a77fbfb565a78c80e754fb59366e8
rhunk <101876869+rhunk@users.noreply.github.com>
2024-06-03 00:51:51 +0200

    chore(constants): update user agent
    Signed-off-by: rhunk <101876869+rhunk@users.noreply.github.com>

--- common/src/main/kotlin/me/rhunk/snapenhance/common/Constants.kt
+++ common/src/main/kotlin/me/rhunk/snapenhance/common/Constants.kt
@@ -3,5 +3,5 @@ package me.rhunk.snapenhance.common
 object Constants {
     val SNAPCHAT_PACKAGE_NAME get() = "com.snapchat.android"
     val SE_PACKAGE_NAME get() = BuildConfig.APPLICATION_ID
-    const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    const val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3"
 }
